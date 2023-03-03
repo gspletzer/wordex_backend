940cf04a786e21e34619d27ada4724585a25fd0b
Grace Spletzer <gspletzer@influxdata.com>
2023-03-03 13:25:19 -0800

    feat: added tests for Score

---
+++ test/game/score_test.exs
@@ -0,0 +1,49 @@
+defmodule Wordex.Game.ScoreTest do
+  use ExUnit.Case
+
+  alias Wordex.Game.Score
+
+  # doctest Score
+
+  describe "new/2" do
+    test "returns Score that contains the answer and the guessed word" do
+      answer = "happy"
+      guess = "patty"
+      result = %Score{answer: answer, guess: guess}
+
+      assert result == Score.new(answer, guess)
+    end
+
+    # test "returns error if guess is less than 5 letters" do
+    # end
+
+    # test "returns error if guess is more than 5 letters" do
+    # end
+  end
+
+  describe "match_greens/1" do
+    test "returns list where tuples with matching x and y values are followed by :green" do
+      result = [
+        {"h", "p", :gray},
+        {"a", "a", :green},
+        {"p", "t", :gray},
+        {"p", "t", :gray},
+        {"y", "y", :green}
+      ]
+
+      assert result == Score.new("happy", "patty") |> Score.match_greens()
+    end
+
+    test "returns list where no tuples display :green if no x and y values match in the tuple" do
+      result = [
+        {"h", "m", :gray},
+        {"a", "o", :gray},
+        {"p", "u", :gray},
+        {"p", "s", :gray},
+        {"y", "e", :gray}
+      ]
+
+      assert result == Score.new("happy", "mouse") |> Score.match_greens()
+    end
+  end
+end
--- test/wordex_test.exs
+++ test/wordex_test.exs
@@ -2,7 +2,7 @@ defmodule WordexTest do
   use ExUnit.Case
   doctest Wordex
 
-  test "greets the world" do
-    assert Wordex.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Wordex.hello() == :world
+  # end
 end
