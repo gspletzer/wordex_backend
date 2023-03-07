442d7f4815b9d4eab6eaeac54ed82e50e0a8f758
Grace Spletzer <gspletzer@influxdata.com>
2023-03-06 18:55:50 -0800

    chore: Clean up spec for Guess

--- lib/game/guess.ex
+++ lib/game/guess.ex
@@ -1,7 +1,7 @@
 defmodule Wordex.Game.Guess do
   @type guess() :: String.t()
 
-  @spec valid_guess?(guess) :: true | false
+  @spec valid_guess?(guess) :: Boolean.t()
   def valid_guess?(guess) do
     # TODO: check valid English word
     with true <- valid_length(guess),
@@ -12,10 +12,12 @@ defmodule Wordex.Game.Guess do
     end
   end
 
+  @spec valid_length(guess) :: Boolean.t()
   defp valid_length(guess) do
     if String.length(guess) == 5, do: true, else: false
   end
 
+  @spec valid_characters(guess) :: Boolean.t()
   defp valid_characters(guess) do
     String.match?(guess, ~r/^[[:alpha:][:lower:]]+$/)
   end
