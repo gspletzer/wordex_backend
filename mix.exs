285a454aa673d9191618700905fbe4b242640e02
Grace Spletzer <gspletzer@influxdata.com>
2023-05-04 16:45:39 -0700

    chore: append app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule WordexBackend.MixProject do
 
   def project do
     [
-      app: :wordex,
+      app: :wordex_backend,
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
